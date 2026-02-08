5511f2a4beeaedfbb2ff7d6a8bf2538f3aeed795
FAIZ_HAWK <faizfuzi57@gmail.com>
2026-02-08 15:47:10 +0800

    Refactor database connection for SSL support
    Updated database connection to use SSL with certifi.

--- database.py
+++ database.py
@@ -1,19 +1,23 @@
+import os
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
-# Format: mysql+pymysql://username:password@localhost:3306/db_name
-# Use an empty password if you haven't set one: "root:@"
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:@localhost:3306/e_portfolio"
+# Gantikan URL dengan yang anda dapat tadi, tetapi kita baiki bahagian SSL
+# Kita gunakan certifi untuk dapatkan path sijil secara automatik
+import certifi
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# Masukkan pautan anda di sini
+# Nota: Saya telah membuang bahagian <CA_PATH> dan menggantikannya dengan pembolehubah certifi
+BASE_URL = "mysql+pymysql://HTMXbv8inEVYUPx.root:OkWgShAjYRS49S1p@gateway01.ap-southeast-1.prod.aws.tidbcloud.com:4000/test"
+
+# Cantumkan dengan konfigurasi SSL yang betul
+SQLALCHEMY_DATABASE_URL = f"{BASE_URL}?ssl_ca={certifi.where()}&ssl_verify_cert=true&ssl_verify_identity=true"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"ssl": {"ca": certifi.where()}}
+)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
